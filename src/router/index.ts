3b38659594529519a05e25a51b82eea0f448682b
VKTRenokh <viktorenokh@gmail.com>
2024-09-25 11:22:37 +0300

    feat: add garage (main) view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('@/views/AppGarage.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: routes
 })
 
 export default router
