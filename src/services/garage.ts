d43adc525f4bd41e0b25ffab721125c376fffcb9
VKTRenokh <viktorenokh@gmail.com>
2025-01-04 14:29:03 +0200

    refactor: start pagination refactor

--- src/services/garage.ts
+++ src/services/garage.ts
@@ -2,3 +2,15 @@ import { baseUrl } from '@/utils/base-url'
 import { mande } from 'mande'
 
 export const garage = mande(baseUrl('/garage'))
+
+export const paginateCars = (
+  page: number,
+  perPage: number
+) => {
+  garage.get({
+    query: {
+      _page: page,
+      _limit: perPage
+    }
+  })
+}
