c6e00ec7b947aec91896a4b6da4e11fa0b60162d
VKTRenokh <viktorenokh@gmail.com>
2025-01-08 14:42:23 +0200

    refactor: remove useless interface

--- src/services/garage.ts
+++ src/services/garage.ts
@@ -3,12 +3,6 @@ import { baseUrl } from '@/utils/base-url'
 import { mande } from 'mande'
 import { createPagination } from '@/utils/create-pagination'
 
-// TODO: Move this into separate file
-export interface PaginationResponse {
-  total: number
-  cars: Car[]
-}
-
 export const garage = mande(baseUrl('/garage'))
 
 export const paginateCars = createPagination<Car>(garage)
